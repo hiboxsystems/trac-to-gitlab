2749a2d7b49e52a96e6ea10c6e6f633b92fdfc8b
Per Lundberg <per.lundberg@hibox.tv>
2018-10-04 09:46:52 +0300

    delete-users.py: New script removing users in GitLab
    Also removed this code from create-missing-users. Its name will now be more in resonance with what the script actually _does_...

--- delete-users.py
+++ delete-users.py
@@ -27,10 +27,6 @@ default_config = {
 config = ConfigParser.ConfigParser(default_config)
 config.read('migrate.cfg')
 
-trac_url = config.get('source', 'url')
-ldap_uid_pattern = config.get('target', 'ldap_uid_pattern')
-project_name = config.get('target', 'project_name')
-
 # NB: This script only supports the 'api' method, not direct database
 # access.
 method = 'api'
@@ -39,25 +35,15 @@ gitlab_url = config.get('target', 'url')
 gitlab_access_token = config.get('target', 'access_token')
 dest_ssl_verify = config.getboolean('target', 'ssl_verify')
 
-from users import users, fork_users, default_groups
+from users import users
 
 if __name__ == "__main__":
-    opts = {
-        'ldap_uid_pattern': ldap_uid_pattern,
-        'default_groups': default_groups
-    }
+    opts = {}
     dest = Connection(gitlab_url, gitlab_access_token, dest_ssl_verify, opts)
 
-    # Create non-existing users
     for user in users:
         gitlab_user = dest.get_user_id(user['username'])
 
-        if gitlab_user == None:
-            print("User does not exist in GitLab: %s, creating..." % user['username'])
-            dest.create_user(user)
-
-    source = xmlrpclib.ServerProxy(trac_url, encoding = 'UTF-8')
-
-    # Create forks of repositories
-    for fork_user in fork_users:
-        dest.create_fork(fork_user, project_name)
+        if gitlab_user:
+            print 'Deleting user %s' % user['username']
+            dest.delete_user(gitlab_user)
